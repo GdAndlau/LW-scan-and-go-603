df973779d577a5d8c59dd8aff1b3f0f6ef04d7e6
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-08-14 16:51:08 +0200

    product info - no frotend, add to basket basic

--- app/controllers/session_items_controller.rb
+++ app/controllers/session_items_controller.rb
@@ -1,2 +1,15 @@
 class SessionItemsController < ApplicationController
+  def create
+    @session_item = SessionItem.new
+    @product_id = Product.find(params[:product_id])
+    @user_sessions = Session.where(user: current_user)
+    @session = @user_sessions.last
+    @session_item.product = @product_id
+    @session_item.session = @session
+    if @session_item.save!
+      redirect_to root_path
+    else
+      render "products/show"
+    end
+  end
 end
