59dc12777f45a469c71219301506696d2b64f60e
Wolfgang Ernst <mail@wolfgangernst.com>
2021-08-14 15:09:36 +0200

    v2

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,12 @@
 class StoresController < ApplicationController
+    def index
+    @stores = Store.all
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @stores.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+  end
 end
